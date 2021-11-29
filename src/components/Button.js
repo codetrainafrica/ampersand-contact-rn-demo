c96cabe01eb09090e137b71c87d0a92133274cfe
Anthony Obaze <toneyobaze@gmail.com>
2021-11-29 12:41:06 +0000

    create Button component

---
+++ src/components/Button.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+
+const Button = ({ text, onPress, containerStyle, textStyle }) => {
+  return (
+    <TouchableOpacity
+      style={[styles.container, containerStyle]}
+      onPress={() => onPress()}
+    >
+      <Text style={[styles.btnText, textStyle]}>{text}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+    borderBottomColor: "#b46754",
+    borderBottomWidth: 2,
+  },
+
+  btnText: {
+    fontSize: 16,
+  },
+});
+
+export default Button;
