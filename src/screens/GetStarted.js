e0b397f8422589be3bface72d0418af53c3bd5b8
Anthony Obaze <toneyobaze@gmail.com>
2021-11-29 12:48:16 +0000

    style GetStarted screen

--- src/screens/GetStarted.js
+++ src/screens/GetStarted.js
@@ -1,10 +1,44 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import Button from "../components/Button";
 
 const GetStarted = () => {
+  const { navigate } = useNavigation();
+
   return (
     <View style={styles.container}>
-      <Text>GetStarted</Text>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require("../../assets/get_started_background.jpeg")}
+          style={styles.image}
+        />
+      </View>
+
+      <View style={styles.detailsContainer}>
+        <View style={styles.introContainer}>
+          <Text style={styles.introText}>
+            KEEP IN TOUCH WITH THE PEOPLE OF AMPERSAND
+          </Text>
+
+          <Text style={styles.mutedText}>
+            Sign in or register with your Ampersand email
+          </Text>
+        </View>
+
+        <View style={styles.btnsContainer}>
+          <Button
+            text="REGISTER"
+            containerStyle={styles.btn}
+            onPress={() => navigate("Register")}
+          />
+          <Button
+            text="SIGN IN"
+            containerStyle={styles.btn}
+            onPress={() => navigate("Login")}
+          />
+        </View>
+      </View>
     </View>
   );
 };
@@ -12,6 +46,47 @@ const GetStarted = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+
+  imageContainer: {
+    flex: 1.5,
+  },
+
+  image: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
+  },
+
+  detailsContainer: {
+    flex: 1,
+    padding: 12,
+  },
+
+  introText: {
+    fontSize: 18,
+    letterSpacing: 1,
+  },
+
+  introContainer: {
+    flex: 1,
+    justifyContent: "space-between",
+  },
+
+  btnsContainer: {
+    flex: 2,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 16,
+  },
+
+  mutedText: {
+    color: "#7f7f7f",
+    letterSpacing: 1,
+  },
+
+  btn: {
+    alignSelf: "center",
   },
 });
 
